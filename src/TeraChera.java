a34a0cace022de991fefdeb26996ed4a66dbe8b2
pradhumnaDsawalkar <pradhumna0007@gmail.com>
2024-02-14 21:37:49 +0530

    Update

---
+++ src/TeraChera.java
@@ -0,0 +1,18 @@
+import Uparwale.SabDiyaHai;
+import java.util.Scanner;
+
+public class TeraChera extends SabDiyaHai {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Duriiii");
+        System.out.println("Phele tum");
+        int a = scanner.nextInt();
+        System.out.println("Kya Mai");
+        int b = scanner.nextInt();
+      //Inhertitance
+        TeraChera teraChera = new TeraChera();
+        teraChera.RishtaWahiSochNai(a,b);
+        teraChera.judaii(a,b);
+
+    }
+}
---
+++ src/Uparwale/SabDiyaHai.java
@@ -0,0 +1,16 @@
+package Uparwale;
+
+public class SabDiyaHai {
+
+    public void RishtaWahiSochNai(int x,int y)
+    {
+        int inkamnam=x+y;
+        System.out.println("HAA BOLA hai BHAI PARTY = "+inkamnam);
+    }
+    public void judaii(int x,int y)
+    {
+        int darddd=x-y;
+
+        System.out.println("NA BOLA hai BHAI DOUBLE PARTY = "+darddd);
+    }
+}
